e7f6a1f747c29405dc38dac6291df6ec1561a4bc
Arnold Perez <alperez@mail.usf.edu>
2019-03-23 22:33:16 -0400

    trimmed highlight face out

--- faces.py
+++ faces.py
@@ -58,10 +58,9 @@ def detect_face(face_file, max_results=4):
         surprise = ('{}'.format(likelihood_name[face.surprise_likelihood]))
         sorrow = ('{}'.format(likelihood_name[face.sorrow_likelihood]))
         emotion = "unknown"
-        probability = "unknown"
         if(anger == 'POSSIBLE' or anger == 'LIKELY' or anger == 'VERY_LIKELY'):
             emotion = "anger"
-            probability = anger
+
 
         if(joy == 'POSSIBLE' or joy == 'LIKELY' or joy == 'VERY_LIKELY'):
             if(emotion != 'unknown'):
@@ -97,44 +96,11 @@ def detect_face(face_file, max_results=4):
     return emotion
 
 
-def highlight_faces(image, faces, output_filename):
-    """Draws a polygon around the faces, then saves to output_filename.
-
-    Args:
-      image: a file containing the image with the faces.
-      faces: a list of faces found in the file. This should be in the format
-          returned by the Vision API.
-      output_filename: the name of the image file to be created, where the
-          faces have polygons drawn around them.
-    """
-    im = Image.open(image)
-    draw = ImageDraw.Draw(im)
-    # Sepecify the font-family and the font-size
-    for face in faces:
-        box = [(vertex.x, vertex.y)
-               for vertex in face.bounding_poly.vertices]
-        draw.line(box + [box[0]], width=5, fill='#00ff00')
-        # Place the confidence value/score of the detected faces above the
-        # detection box in the output image
-        draw.text(((face.bounding_poly.vertices)[0].x,
-                   (face.bounding_poly.vertices)[0].y - 30),
-                  str(format(face.detection_confidence, '.3f')) + '%',
-                  fill='#FF0000')
-    im.save(output_filename)
-# [END vision_face_detection_tutorial_process_response]
-
-
-
-
-
 # [START vision_face_detection_tutorial_run_application]
 def main(input_filename, output_filename, max_results):
     with open(input_filename, 'rb') as image:
         emotion = detect_face(image, max_results)
         print(emotion)
-
-
-        print('Writing to file {}'.format(output_filename))
         # Reset the file pointer, so we can read the file again
         image.seek(0)
         #highlight_faces(image, faces, output_filename)
